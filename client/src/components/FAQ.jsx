d55c3fdfa09c8bb79f5b7266e0b50af601914db9
Vinh Nguyen <96484600+vinntt@users.noreply.github.com>
2022-11-14 23:41:23 +0100

    adjust styles and replace the photo

--- client/src/components/FAQ.jsx
+++ client/src/components/FAQ.jsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import React from "react";
 import { styled } from "@mui/material/styles";
@@ -25,10 +25,10 @@ const AccordionSummary = styled((props) => (
     {...props}
   />
 ))(({ theme }) => ({
-  backgroundColor:
-    theme.palette.mode === "dark"
-      ? "rgba(255, 255, 255, .05)"
-      : "rgba(0, 0, 0, .03)",
+  backgroundColor: "#F8F9FF",
+  // theme.palette.mode === "dark"
+  //   ? "rgba(255, 255, 255, .05)"
+  //   : "rgba(0, 0, 0, .03)",
   flexDirection: "row-reverse",
   "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
     transform: "rotate(90deg)",
@@ -41,6 +41,7 @@ const AccordionSummary = styled((props) => (
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
   borderTop: "1px solid rgba(0, 0, 0, .125)",
+  backgroundColor: "#F8F9FF",
 }));
 
 export default function FAQ() {
@@ -52,11 +53,12 @@ export default function FAQ() {
 
   return (
     <Container
+      id="faq"
       maxWidth="xl"
-      style={{ backgroundColor: "#F8F9FF", padding: "5rem 5rem" }}
+      style={{ backgroundColor: "#F8F9FF", padding: "5rem 3rem" }}
     >
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid container xs={7} style={{ padding: "0 4rem" }}>
+        <Grid container xs={6} style={{ padding: "0 4rem" }}>
           <Typography
             variant="h4"
             align="left"
@@ -65,14 +67,15 @@ export default function FAQ() {
             Is this the right program for you?
           </Typography>
           <Grid item>
-            <Typography variant="h6" align="left">
-              Eget augue in et ac sagittis senectus sit maecenas tristique.
-              Pellentesque cras proin quis leo maecenas sed cursus. In eu diam
-              metus odio nisl at. At tortor sollicitudin at sem. Leo ornare
-              lectus hac.
+            <Typography variant="" align="left">
+              This program is for people with forced migration background who
+              want to step into Germany’s booming tech industry in Berlin. For
+              the duration of the course you’ll learn in person in Berlin and
+              spend a minimum of 25 hours a week for learning, classes and
+              events.
             </Typography>
           </Grid>
-          <Grid item style={{ padding: "3rem 2rem 1rem 0"}}>
+          <Grid item style={{ padding: "3rem 2rem 1rem 0" }}>
             <Accordion
               expanded={expanded === "panel1"}
               onChange={handleChange("panel1")}
@@ -81,15 +84,15 @@ export default function FAQ() {
                 aria-controls="panel1d-content"
                 id="panel1d-header"
               >
-                <Typography>Collapsible Group Item #1</Typography>
+                <Typography variant="h6">What do I need to get started?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
+                  At Techstart we want to understand why you are motivated to
+                  join our program. We are building a vibrant and
+                  interdisciplinary team, so don’t hesitate to apply independent
+                  of your study background and previous knowledge. There is no
+                  such thing as the right answer to our questions.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -101,7 +104,7 @@ export default function FAQ() {
                 aria-controls="panel2d-content"
                 id="panel2d-header"
               >
-                <Typography>Collapsible Group Item #2</Typography>
+                <Typography variant="h6">Which documents do I need?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
@@ -121,7 +124,27 @@ export default function FAQ() {
                 aria-controls="panel3d-content"
                 id="panel3d-header"
               >
-                <Typography>Collapsible Group Item #3</Typography>
+                <Typography variant="h6">How do I apply?</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                  eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion
+              expanded={expanded === "panel4"}
+              onChange={handleChange("panel4")}
+            >
+              <AccordionSummary
+                aria-controls="panel34-content"
+                id="panel34-header"
+              >
+                <Typography variant="h6">Do I need to have a degree?</Typography>
               </AccordionSummary>
               <AccordionDetails>
                 <Typography>
@@ -136,7 +159,7 @@ export default function FAQ() {
           </Grid>
           <Grid item>
             <Button
-              href="/"
+              href="/application"
               type="submit"
               fullWidth
               variant="button"
@@ -155,21 +178,19 @@ export default function FAQ() {
             </Button>
           </Grid>
         </Grid>
-        <Grid
-          container
-          xs={4}
-          position="relative"
-          style={{ padding: "0 2rem" }}
-        >
-          <iframe
-            width="100%"
-            height="100%"
-            src="https://www.youtube.com/embed/AZMxLOQe-6A"
-            title="Onboarding Week Techstart program 2022"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
+        <Grid xs={6} position="relative" style={{ padding: "3rem" }}>
+          <Box
+          href="/application"
+            component="img"
+            sx={{
+              height: "auto",
+              maxWidth: "100%",
+              // minHeight: { xs: "250", md: 0 },
+              // maxHeight: { xs: 0, md: "889%" },
+            }}
+            alt="FAQ"
+            src="https://res.cloudinary.com/dymdurqej/image/upload/v1668115106/techstart/student-ts22/220706_OBW_photo_Rosa_Reibke_5_ovgm0z.jpg"
+          />
         </Grid>
       </Grid>
     </Container>
