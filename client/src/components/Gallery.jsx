64ab541b057ed18ca30a0071cc064e6b37199088
Vinh Nguyen <96484600+vinntt@users.noreply.github.com>
2022-11-14 23:29:29 +0100

    create the  gallery component

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import { Container } from '@mui/system';
+import * as React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { Container } from "@mui/system";
 
 const srcset = (image, size, rows = 1, cols = 1) => {
   return {
@@ -10,67 +10,101 @@ const srcset = (image, size, rows = 1, cols = 1) => {
       size * rows
     }&fit=crop&auto=format&dpr=2 2x`,
   };
-}
-
+};
 
 const galleryImgs = [
-    {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      title: 'Breakfast',
-      rows: 2,
-      cols: 2,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'Burger',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'Camera',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-      title: 'Coffee',
-      cols: 2,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-      title: 'Hats',
-      cols: 2,
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-      title: 'Honey',
-      author: '@arwinneil',
-      rows: 2,
-      cols: 2,
-    },
-  ];
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668115755/techstart/student-ts22/1667380555659_kytq9n.jpg",
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668115269/techstart/student-ts22/220706_OBW_photo_Rosa_Reibke_38_rvrosh.jpg",
+    title: "Burger",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668115245/techstart/student-ts22/220706_OBW_photo_Rosa_Reibke_67_yrczt7.jpg",
+    title: "Honey",
+    rows: 2,
+    cols: 3,
+  },
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668114330/techstart/student-ts22/IMG_20221020_142530_uhjvky.jpg",
+    title: "Hats",
+    rows: 2,
+    cols: 1,
+  },
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668115322/techstart/student-ts22/220706_OBW_photo_Rosa_Reibke_24_ugri4p.jpg",
+    title: "Camera",
+    rows: 2,
+    cols: 3,
+  },
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668115806/techstart/student-ts22/IMG_20220909_134433_wcduzj.jpg",
+    title: "Coffee",
+    rows: 2,
+    cols: 3,
+  },
 
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668115151/techstart/student-ts22/220706_OBW_photo_Rosa_Reibke_47_bnbhfb.jpg",
+    title: "Hats",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668115294/techstart/student-ts22/220928_Disc_Golf_photo_Techstart_2_fzlrwl.jpg",
+    title: "Honey",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668114921/techstart/student-ts22/Laptop_Donation3_photo_heyjobs_wzvi3o.jpg",
+    title: "Honey",
+    rows: 2,
+    cols: 3,
+  },
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668114853/techstart/student-ts22/4de7acff-a886-4710-a1a4-7178cdaa78ef_vujnhx.jpg",
+    title: "Honey",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://res.cloudinary.com/dymdurqej/image/upload/v1668115049/techstart/student-ts22/220706_OBW_photo_Rosa_Reibke_26_axf6hk.jpg",
+    title: "Hats",
+    rows: 2,
+    cols: 1,
+  },
+];
 
 export default function QuiltedImageList() {
   return (
-    <Container
-      maxWidth="xl"
-      style={{ padding: "5rem 0"}}
-    >
-<ImageList
-      sx={{ width: "100%", height: "100%" }}
-      variant="quilted"
-      cols={8}
-    //   rowHeight={121}
-    >
-      {galleryImgs.map((item) => (
-        <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-          <img
-            {...srcset(item.img, 121, item.rows, item.cols)}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <Container maxWidth="xl">
+      <ImageList
+        sx={{ width: "100%", height: "100%" }}
+        variant="quilted"
+        cols={8}
+        rowHeight={120}
+      >
+        {galleryImgs.map((item) => (
+          <ImageListItem
+            key={item.img}
+            cols={item.cols || 1}
+            rows={item.rows || 1}
+          >
+            <img
+              {...srcset(item.img, item.rows, item.cols)}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </Container>
   );
 }
-
