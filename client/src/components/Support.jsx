02864832b568a6da4554f326cef114e11295458f
Vinh Nguyen <96484600+vinntt@users.noreply.github.com>
2022-11-14 23:35:52 +0100

    adjust styles for cards, icon and text

--- client/src/components/Support.jsx
+++ client/src/components/Support.jsx
@@ -1,154 +1,216 @@
 import React from "react";
 import Button from "@mui/material/Button";
-// import { buildUrl, Transformer } from "cloudinary-build-url";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import {
-  Box,
-  Card,
-  CardMedia,
-  CardContent,
-  CardActions,
-  CardHeader,
-  Avatar,
-} from "@mui/material";
-import { purple } from "@mui/material/colors";
-import AddIcon from "@mui/icons-material/Add";
+import { Card, CardContent, CardHeader, Avatar, Box } from "@mui/material";
+import PaidIcon from "@mui/icons-material/Paid";
+import VoiceChatIcon from "@mui/icons-material/VoiceChat";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 
 function Support() {
   return (
-    <Container maxWidth="xl" style={{ padding: "3rem 10rem" }}>
+    <Container maxWidth="xl" style={{ padding: "6rem" }}>
       <Typography
         variant="h3"
         align="center"
-        style={{ fontWeight: "bold", padding: "4rem 0" }}
+        style={{ fontWeight: "bold", padding: "3rem 0" }}
       >
         Support us
       </Typography>
-      <Typography
-        variant="h6"
-        align="center"
+      <Grid
+        container
+        xs={10}
+        spacing={2}
+        sx={{
+          rowGap: "1rem",
+          margin: "auto",
+        }}
       >
-        Eget augue in et ac sagittis senectus sit maecenas tristique.
-        Pellentesque cras proin quis leo maecenas sed cursus. In eu diam metus
-        odio nisl at. At tortor sollicitudin at sem. Leo ornare lectus hac.
-      </Typography>
-      <Grid container sx={{ gap: "2rem", paddingTop: "2rem" }}>
-        {/* Card 1 */}
-        <Card sx={{ maxWidth: 345, boxShadow: 4, borderRadius: "12px", backgroundColor: "#F8F9FF"  }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: purple[500] }}>
-                <AddIcon />
-              </Avatar>
-            }
-            title="FINANCIALLY"
-            subheader="Sponsor a student for the program"
-          />
+        <Typography variant="h6" align="center">
+          Eget augue in et ac sagittis senectus sit maecenas tristique.
+          Pellentesque cras proin quis leo maecenas sed cursus. In eu diam metus
+          odio nisl at. At tortor sollicitudin at sem. Leo ornare lectus hac.
+        </Typography>
+        <Grid
+          container
+          sx={{ gap: "2rem", paddingTop: "2rem", justifyContent: "center" }}
+        >
+          {/* Card 1 */}
+          <Card
+            sx={{
+              maxWidth: 345,
+              boxShadow: "0px 4px 0px #000000",
+              border: "2px solid #0A1025",
+              borderRadius: "16px",
+              backgroundColor: "#F8F9FF",
+              textAlign: "left",
+            }}
+          >
+            <CardHeader
+              avatar={
+                <Avatar
+                  sx={{
+                    bgcolor: "black",
+                    color: "white",
+                    border: "3px solid #FFFFFF",
+                    width: 70,
+                    height: 70,
+                    boxShadow: "0px 2px 0px #030211",
+                  }}
+                >
+                  <PaidIcon />
+                </Avatar>
+              }
+              titleTypographyProps={{ variant: "h6", fontWeight: "900" }}
+              title="FINANCIALLY"
+              subheader="Sponsor a student for the program"
+            />
 
-          <CardContent style={{}}>
-            <Typography variant="body2" align="left" color="text.secondary">
-            Eget augue in et ac sagittis senectus sit maecenas tristique.
-              Pellentesque cras proin quis leo maecenas sed cursus. In eu diam
-              metus odio nisl at. At tortor sollicitudin at sem. Leo ornare
-              lectus hac.
-            </Typography>
-            <Button
-              href="/"
-              type="submit"
-            //   fullWidth = "false"
-              variant="button"
-              sx={{ mt: 3, mb: 2, py: 1 }}
-              style={{
-                backgroundColor: "#915FC7",
-                border: "2px solid black",
-                color: "#FFFFFF",
-                fontWeight: "bold",
-                boxShadow: "0px 2px 0px #000000",
-                borderRadius: "12px",
-              }}
-            >
-              Get in touch
-            </Button>
-          </CardContent>
-        </Card>
-        {/* Card 2 */}
-        <Card sx={{ maxWidth: 345, boxShadow: 4, borderRadius: "12px", backgroundColor: "#F8F9FF"  }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: purple[500] }} aria-label="recipe">
-                <AddIcon />
-              </Avatar>
-            }
-            title="GIVING A TALK"
-            subheader="Reaching out to people and inspire"
-          />
+            <CardContent style={{ padding: "0 16px" }}>
+              <Typography variant="body1" align="left">
+                Eget augue in et ac sagittis senectus sit maecenas tristique.
+                Pellentesque cras proin quis leo maecenas sed cursus. In eu diam
+                metus odio nisl at. At tortor sollicitudin at sem. Leo ornare
+                lectus hac.
+              </Typography>
+              <Box style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  href="/"
+                  type="submit"
+                  variant="button"
+                  sx={{ mt: 3, mb: 2, py: 1 }}
+                  style={{
+                    backgroundColor: "#915FC7",
+                    border: "2px solid black",
+                    color: "#FFFFFF",
+                    fontWeight: "bold",
+                    boxShadow: "0px 2px 0px #000000",
+                    borderRadius: "12px",
+                  }}
+                >
+                  Become a Supporter
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+          {/* Card 2 */}
+          <Card
+            sx={{
+              maxWidth: 345,
+              boxShadow: "0px 4px 0px #000000",
+              border: "2px solid #0A1025",
+              borderRadius: "16px",
+              backgroundColor: "#F8F9FF",
+              textAlign: "left",
+            }}
+          >
+            <CardHeader
+              avatar={
+                <Avatar
+                  sx={{
+                    bgcolor: "black",
+                    color: "white",
+                    border: "3px solid #FFFFFF",
+                    width: 70,
+                    height: 70,
+                    boxShadow: "0px 2px 0px #030211",
+                  }}
+                >
+                  <VoiceChatIcon />
+                </Avatar>
+              }
+              titleTypographyProps={{ variant: "h6", fontWeight: "900" }}
+              title="GIVING A TALK"
+              subheader="Reaching out to people and inspire"
+            />
 
-          <CardContent style={{}}>
-            <Typography variant="body2" align="left" color="text.secondary">
-            Eget augue in et ac sagittis senectus sit maecenas tristique.
-              Pellentesque cras proin quis leo maecenas sed cursus. In eu diam
-              metus odio nisl at. At tortor sollicitudin at sem. Leo ornare
-              lectus hac.
-            </Typography>
-            <Button
-              href="/"
-              type="submit"
-            //   fullWidth = "false"
-              variant="button"
-              sx={{ mt: 3, mb: 2, py: 1 }}
-              style={{
-                backgroundColor: "#915FC7",
-                border: "2px solid black",
-                color: "#FFFFFF",
-                fontWeight: "bold",
-                boxShadow: "0px 2px 0px #000000",
-                borderRadius: "12px",
-              }}
-            >
-              Get in touch
-            </Button>
-          </CardContent>
-        </Card>
-        {/* Card 3 */}
-        <Card sx={{ maxWidth: 345, boxShadow: 4, borderRadius: "12px", backgroundColor: "#F8F9FF"  }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: purple[500] }} aria-label="recipe">
-                <AddIcon />
-              </Avatar>
-            }
-            title="HIRING/ INTERNSHIPS"
-            subheader="Offer to students"
-          />
+            <CardContent style={{ padding: "0 16px" }}>
+              <Typography variant="body1" align="left">
+                Eget augue in et ac sagittis senectus sit maecenas tristique.
+                Pellentesque cras proin quis leo maecenas sed cursus. In eu diam
+                metus odio nisl at. At tortor sollicitudin at sem. Leo ornare
+                lectus hac.
+              </Typography>
+              <Box style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  href="/"
+                  type="submit"
+                  variant="button"
+                  sx={{ mt: 3, mb: 2, py: 1 }}
+                  style={{
+                    backgroundColor: "#915FC7",
+                    border: "2px solid black",
+                    color: "#FFFFFF",
+                    fontWeight: "bold",
+                    boxShadow: "0px 2px 0px #000000",
+                    borderRadius: "12px",
+                  }}
+                >
+                  Become a Supporter
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+          {/* Card 3 */}
+          <Card
+            sx={{
+              maxWidth: 345,
+              boxShadow: "0px 4px 0px #000000",
+              border: "2px solid #0A1025",
+              borderRadius: "16px",
+              backgroundColor: "#F8F9FF",
+              textAlign: "left",
+            }}
+          >
+            <CardHeader
+              avatar={
+                <Avatar
+                  sx={{
+                    bgcolor: "black",
+                    color: "white",
+                    border: "3px solid #FFFFFF",
+                    width: 70,
+                    height: 70,
+                    boxShadow: "0px 2px 0px #030211",
+                  }}
+                >
+                  <BusinessCenterIcon />
+                </Avatar>
+              }
+              titleTypographyProps={{ variant: "h6", fontWeight: "900" }}
+              title="HIRING/ INTERNSHIPS"
+              subheader="Offer to students"
+            />
 
-          <CardContent style={{}}>
-            <Typography variant="body2" align="left" color="text.secondary">
-            Eget augue in et ac sagittis senectus sit maecenas tristique.
-              Pellentesque cras proin quis leo maecenas sed cursus. In eu diam
-              metus odio nisl at. At tortor sollicitudin at sem. Leo ornare
-              lectus hac.
-            </Typography>
-            <Button
-              href="/"
-              type="submit"
-            //   fullWidth = "false"
-              variant="button"
-              sx={{ mt: 3, mb: 2, py: 1 }}
-              style={{
-                backgroundColor: "#915FC7",
-                border: "2px solid black",
-                color: "#FFFFFF",
-                fontWeight: "bold",
-                boxShadow: "0px 2px 0px #000000",
-                borderRadius: "12px",
-              }}
-            >
-              Get in touch
-            </Button>
-          </CardContent>
-        </Card>
+            <CardContent style={{ padding: "0 16px" }}>
+              <Typography variant="body1" align="left">
+                Eget augue in et ac sagittis senectus sit maecenas tristique.
+                Pellentesque cras proin quis leo maecenas sed cursus. In eu diam
+                metus odio nisl at. At tortor sollicitudin at sem. Leo ornare
+                lectus hac.
+              </Typography>
+              <Box style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  href="/"
+                  type="submit"
+                  variant="button"
+                  sx={{ mt: 3, mb: 2, py: 1 }}
+                  style={{
+                    backgroundColor: "#915FC7",
+                    border: "2px solid black",
+                    color: "#FFFFFF",
+                    fontWeight: "bold",
+                    boxShadow: "0px 2px 0px #000000",
+                    borderRadius: "12px",
+                  }}
+                >
+                  Become a Supporter
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </Container>
   );
