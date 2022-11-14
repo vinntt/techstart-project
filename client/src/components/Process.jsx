52ca92433307ee057c745db9ed7d514dc08c2d5f
Vinh Nguyen <96484600+vinntt@users.noreply.github.com>
2022-11-14 23:40:38 +0100

    create the application process component

--- client/src/components/Process.jsx
+++ client/src/components/Process.jsx
@@ -1,51 +1,91 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Stepper from "@mui/material/Stepper";
-import Step from "@mui/material/Step";
-import StepLabel from "@mui/material/StepLabel";
-import StepContent from "@mui/material/StepContent";
 import Button from "@mui/material/Button";
-import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
-import { useState } from "react";
-import { Container } from "@mui/material";
+import { Avatar, Container, Grid } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
+import ThreePIcon from "@mui/icons-material/ThreeP";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
 
 const steps = [
   {
     label: "Written application",
     description: `In the written application you must send us your CV and answer a short questionnaire about your
     motivation, background, and expectations`,
+    orientation: "right",
+    icon: <EditIcon />,
+    link: true,
   },
   {
     label: "Challenge",
     description:
       "We invite you to campus for an admission day where you will join in some practical team challenges. In the event that Corona does not allow us to meet in person, we will send you a virtual challenge that you must complete",
+    orientation: "left",
+    icon: <TipsAndUpdatesIcon />,
+    link: true,
   },
   {
     label: "Interview",
     description: `In the online interview we want to get to know you better, but this is also your chance to get to know us.`,
+    orientation: "right",
+    icon: <ThreePIcon />,
+    link: true,
   },
   {
     label: "Confirmation",
     description: `After admission day, you will receive your confirmation within two weeks.`,
+    orientation: "left",
+    icon: <GroupAddIcon />,
   },
 ];
 
+function stepAvatar({ icon, link }) {
+  return (
+    <Grid item xs={2} md={2} style={{ position: "relative" }}>
+      <Avatar
+        sx={{
+          bgcolor: "black",
+          color: "white",
+          border: "3px solid #FFFFFF",
+          width: 70,
+          height: 70,
+          boxShadow: "0px 2px 0px #030211",
+          zIndex: "1000"
+        }}
+      >
+        {icon}
+      </Avatar>
+      {link && (
+        <Divider
+          orientation="vertical"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: "27%",
+            height: "100%",
+            border: "3px solid #0A1025",
+          }}
+        />
+      )}
+    </Grid>
+  );
+}
+
+function stepStyle(orientation) {
+  let style = {
+    margin: "0 auto",
+  };
+
+  if (orientation === "right") {
+    style.justifyContent = "right";
+  }
+
+  return style;
+}
+
 export default function Process() {
-  const [activeStep, setActiveStep] = useState(0);
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
   return (
     <Container
       maxWidth="xl"
@@ -58,58 +98,26 @@ export default function Process() {
       >
         Application Process
       </Typography>
-      <Box sx={{ maxWidth: 400 }}>
-        <Stepper activeStep={activeStep} orientation="vertical">
-          {steps.map((step, index) => (
-            <Step key={step.label}>
-              <StepLabel
-                optional={
-                  index === 2 ? (
-                    <Typography variant="caption">Last step</Typography>
-                  ) : null
-                }
-              >
-                {step.label}
-              </StepLabel>
-              <StepContent>
-                <Typography>{step.description}</Typography>
-                <Box sx={{ mb: 2 }}>
-                  <div>
-                    <Button
-                      variant="contained"
-                      onClick={handleNext}
-                      sx={{ mt: 1, mr: 1 }}
-                    >
-                      {index === steps.length - 1 ? "Finish" : "Continue"}
-                    </Button>
-                    <Button
-                      disabled={index === 0}
-                      onClick={handleBack}
-                      sx={{ mt: 1, mr: 1 }}
-                    >
-                      Back
-                    </Button>
-                  </div>
-                </Box>
-              </StepContent>
-            </Step>
-          ))}
-        </Stepper>
-        {activeStep === steps.length && (
-          <Paper square elevation={0} sx={{ p: 3 }}>
-            <Typography>All steps completed - you&apos;re finished</Typography>
-            <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
-              Reset
-            </Button>
-          </Paper>
-        )}
-
+     <Grid style={{paddingBottom: "4rem"}}>
+     {steps.map((step) => (
+        <Grid container xs="6" style={stepStyle(step.orientation)}>
+          {step.orientation === "right" && stepAvatar(step)}
+          <Grid item xs={5} md={5}>
+            <Typography variant="h6" style={{ fontWeight: "bold" }}>
+              {step.label}
+            </Typography>
+            <Typography variant="body1">{step.description}</Typography>
+          </Grid>
+          {step.orientation !== "right" && stepAvatar(step)}
+        </Grid>
+      ))}
+     </Grid>
+      <Box>
         <Button
-          href="/"
+          href="/application"
           type="submit"
           variant="button"
           sx={{ mt: 3, mb: 2, py: 1 }}
-          endIcon={""}
           style={{
             backgroundColor: "#F1005F",
             border: "2px solid black",
