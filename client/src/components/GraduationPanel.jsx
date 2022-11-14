e9d8d3e2e3544d95c1b566a538bd943cb5791ac5
Vinh Nguyen <96484600+vinntt@users.noreply.github.com>
2022-11-14 23:29:53 +0100

    change the padding

--- client/src/components/GraduationPanel.jsx
+++ client/src/components/GraduationPanel.jsx
@@ -1,11 +1,11 @@
-import { Container, Typography } from "@mui/material";
+import {Container, Typography } from "@mui/material";
 import React from "react";
 
 export default function GraduationPanel() {
   return (
     <Container
       maxWidth="xl"
-      style={{ padding: "5rem", backgroundColor: "#5C2E91" }}
+      style={{ padding: "5rem 0rem", backgroundColor: "#5C2E91" }}
     >
       <Typography variant="h4" align="center" style={{ fontWeight: "bold", color: "white" }}>
         75% of our graduates are working within six months
