04670dd5ffa36f5b65ec26e1f8c94638c7f3a495
Vinh Nguyen <96484600+vinntt@users.noreply.github.com>
2022-11-14 23:28:20 +0100

    add scroll to fragment on same page feature

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { scrollToFragment } from "scroll-to-fragment";
+import { createBrowserHistory } from "history";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,5 +15,19 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+scrollToFragment({
+  // customize the target of a given fragment ID (default is getElementById):
+  getElement: (fragmentId) => document.getElementsByName(fragmentId)[0],
+
+  // adjust the scroll position after history PUSH events:
+  history: createBrowserHistory(),
+
+  // customize scrolling behavior:
+  scrollIntoView: (element) => {
+    console.log(element);
+    element.scrollIntoView({ behavior: "smooth" })
+  },
+});
 
 reportWebVitals();
